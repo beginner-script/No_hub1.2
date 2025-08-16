8171a446b6bf46655968bc1b486577b96e18456d
No_dev <bagsinjae80@gmail.com>
2025-08-16 17:34:11 +0900

    .lua

--- .lua
+++ .lua
@@ -122,14 +122,14 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/beginner-script/click
 }) 
 
 local Button = mainTab:CreateButton({
-   Name = "왔다 갔다 반복 (No_1)",
+   Name = "대쉬 (No_1)",
    Callback = function()
 loadstring(game:HttpGet("https://mystb.in/raw/6e6eeb04932b253609/1"))() 
    end,
 }) 
 
 local Button = mainTab:CreateButton({
-   Name = "대쉬 q (No_1)",
+   Name = "자동 테러 (No_1)",
    Callback = function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/beginner-script/tweentp-/refs/heads/main/.lua"))()   
    end,
